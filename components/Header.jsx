cae56343a601f7e9b253d901e64108dbcab41a55
aqazza <anasqazza@gmail.com>
2024-08-24 22:31:56 -0700

    Header changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,31 +1,29 @@
 "use client";
 import React, { useContext } from "react";
 import { CursorContext } from "./CursorContext";
-
 import { motion } from "framer-motion";
-
+import { AiOutlineMenu } from "react-icons/ai"; // Import the AiOutlineMenu icon
 
 const Header = () => {
-    const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
-    return (
-        <header className="pb-6 xl:pb-[50px] fixed z-40 w-full bg-accent-100
-        xl:bg-transparent">
-    {/* topbar */}
-    <div className="bg-secondary-100 mb-6 xl:mb-[50px] xl:h-[50px]
-    py-4 xl:py-0">
+  const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
+  const [mobileNav, setMobileNav] = useState(false);
+  return (
+    <header className="pb-6 xl:pb-[50px] fixed z-40 w-full bg-accent-100 xl:bg-transparent">
+      {/* topbar */}
+      <div className="bg-secondary-100 mb-6 xl:mb-[50px] xl:h-[50px] py-4 xl:py-0">
         topbar
-    </div>
-    <div className="container">
+      </div>
+      <div className="container mx-auto flex items-center justify-between px-6">
         {/* logo */}
+        <div></div>
+        {/* mobile nav trigger */}
         <div>
+          <AiOutlineMenu className="text-3x text-primary" />{" "}
+          {/* Removed the semicolon from className */}
         </div>
-    {/* mobile nav trigger */}
-    <div>
-        <AiOutlineMenu className="text-3x; text-primary" />
-    </div>
-    </div>
+      </div>
     </header>
-    );
+  );
 };
 
 export default Header;
