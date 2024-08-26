836ac75c9ba29114e4cac9bbb224332058f9cbc2
aqazza <anasqazza@gmail.com>
2024-08-26 11:31:20 -0700

    completed form page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,8 +2,8 @@
 import { motion } from "framer-motion";
 import { useContext } from "react";
 import Image from "next/image";
-import { CursorContext } from "./CursorContext";
-
+import { CursorContext } from "@/components/CursorContext";
+import Form from "@/components/Form";
 
 const Contact = () => {
   const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
@@ -11,10 +11,17 @@ const Contact = () => {
     <motion.section
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { delay: 2 } }}
-className="min-h-screen flex items-center overflow-x-hidden"
->
-  contact page
-</motion.section> 
+      className="min-h-screen flex items-center overflow-x-hidden"
+    >
+      <div className = "container mx-auto pt-48 pb-12 xl:pt-32 xl:pb-0">
+        <div className="flex flex-col gap-12 xl:flex-row h-full">
+<div>text</div>   
+<div>
+  <Form />
+  </div>
+</div>
+      </div>
+  </motion.section>
   );
 };
 
